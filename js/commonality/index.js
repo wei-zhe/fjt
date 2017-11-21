51b7f66bf60cf2ebd882ea9a6cece807fca83b57
weizhe <2270734520@qq.com>
2017-11-21 16:02:25 +0800

    sss

--- js/commonality/index.js
+++ js/commonality/index.js
@@ -3,10 +3,5 @@ $(function(){
         console.log(data);
         $( "#app" ).html( data );
     });
-    $('.top').bind('click',function(){
-        $.get( "home.html", function( data ) {
-            console.log(data);
-            $( "#app" ).html( data );
-        });
-    })
+    
 })  
