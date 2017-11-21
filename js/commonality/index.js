063e193b161a594791cbba5c2971f58b48de4159
Ade <2270734250@qq.com>
2017-11-21 15:41:37 +0800

    Update index.js

--- js/commonality/index.js
+++ js/commonality/index.js
@@ -2,7 +2,7 @@ $(function(){
     $('#app').click(function(){
         $.get( "home.html", function( data ) {
             console.log(data);
-            $( ".app" ).html( data );
+            $( "#app" ).html( data );
         });
     })
 })  
