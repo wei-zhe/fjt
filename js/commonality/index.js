fbdc35b421101a7f72faceefc275b505d56fa491
weizhe <2270734520@qq.com>
2017-11-23 16:16:28 +0800

    sss

--- js/commonality/index.js
+++ js/commonality/index.js
@@ -1,4 +1,8 @@
-$(function(){  
+$(function(){
+    var app_height = document.body.clientHeight - 50;
+    $('#app').css({
+        'min-hieght': app_height,
+    }) 
     $.get( "navbar.html", function( data ) {
         console.log(data);
         $( "#navs" ).html( data );
