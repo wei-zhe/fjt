9e3131dd172e18d031df0141c36ae7dcf33f3954
Ade <2270734250@qq.com>
2019-07-24 16:28:01 +0800

    no-message

---
+++ pages-VUE/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h3>
+      Hi
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
